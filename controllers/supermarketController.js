149ca700f507605fb2fa52de1d31a7f3d23ed067
Julian <juferus12@gmail.com>
2019-11-05 11:22:35 -0500

    CRUD Categorias

--- controllers/supermarketController.js
+++ controllers/supermarketController.js
@@ -3,6 +3,7 @@
 const SupermarketModel = require('../models/supermarketSchema')
 
 class Supermarket {
+
     async create(data) {
         const isExist = await SupermarketModel.get({ address: data.address })
         if (!isExist._id) {
@@ -21,6 +22,20 @@ class Supermarket {
         } else {
             return { error: 'No se ha actualizado' }
         }
+    }
+
+    async detail(data) {
+        const supermarket = await SupermarketModel.get({ _id: data })
+        if (supermarket._id) {
+            return supermarket
+        } else {
+            return { error: 'El supermercado no existe' }
+        }
+    }
+
+    async detailAll() {
+        const getAll = await SupermarketModel.search({})
+        return getAll
     }
 
 }
--- urls/routes.js
+++ urls/routes.js
@@ -8,11 +8,13 @@ const auth = require('./auth')
 const availability = require('./availability')
 const supermarket = require('./supermarket')
 const product = require('./product')
+const category = require('./category')
 
 routes.use('/user', user)
 routes.use('/auth', auth)
 routes.use('/availability', availability)
 routes.use('/supermarket', supermarket)
 routes.use('/product', product)
+routes.use('/category', category)
 
 module.exports = routes
