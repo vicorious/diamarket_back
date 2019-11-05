2d3386bc20af1fbfc282f85daa63604e205f8798
Jairo <jairoandres689@gmail.com>
2019-11-05 10:56:33 -0500

    Create product

--- urls/product.js
+++ urls/product.js
@@ -3,9 +3,11 @@
 const express = require('express')
 const asyncify = require('express-asyncify')
 const routes = asyncify(express.Router())
+const ProductController = require('../controllers/productController')
 
 routes.post('/create', async(request, response) => {
-
+    const create = await ProductController.create(request.body)
+    response.json(create)
 })
 
 module.exports = routes
