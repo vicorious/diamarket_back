2d60b7cab6ac3332221b8f9e9398e3fbee8d8260
NicolasSalazar <desarrollo@gracialab.co>
2020-01-09 20:05:21 -0500

    ultimate documentation in swagger the userlist

--- urls/userList.js
+++ urls/userList.js
@@ -68,7 +68,6 @@ const { isClient } = require('../middleware/token')
  *                type: string
  *                example: "Ya existe una lista con este nombre"
  */
-
 routesUserListApp.post('', isClient, async (request, response) => {
   request.body.user = request.User.id
   const data = request.body
@@ -76,6 +75,60 @@ routesUserListApp.post('', isClient, async (request, response) => {
   response.json(create)
 })
 
+/**
+ * @swagger
+ * /app/userlist/{id}:
+ *  put:
+ *    tags:
+ *      - UserList
+ *    description: En este endpoint se actualiza una lista de un usuario
+ *    produces:
+ *    - applications/json
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      requiered: true
+ *    - in: header
+ *      name: Authorization
+ *      type: string
+ *      required: true
+ *    - in: body
+ *      name: body
+ *      schema:
+ *        $ref: '#/definitions/UserList'
+ *      responses:
+ *        200:
+ *          description: Actualiza una lista de productos
+ *          schema:
+ *            properties:
+ *              estado:
+ *                type: boolean
+ *                example: true
+ *              data:
+ *                type: object
+ *                properties:
+ *                  update:
+ *                    type: boolean
+ *                    example: true
+ *              mensaje:
+ *                type: string
+ *                example: null
+ *        400:
+ *          description: Error al crear el usuario
+ *          schema:
+ *            properties:
+ *              estado:
+ *                type: boolean
+ *                example: false
+ *              data:
+ *                type: array
+ *                items:
+ *                  type: string
+ *                  example: "array vacio"
+ *              mensaje:
+ *                type: string
+ *                example: "No exite la lista de usuario"
+ */
 routesUserListApp.put('/:id', isClient, async (request, response) => {
   const id = request.params.id
   const data = request.body
@@ -83,12 +136,101 @@ routesUserListApp.put('/:id', isClient, async (request, response) => {
   response.json(update)
 })
 
+/**
+ * @swagger
+ * /app/userlist:
+ *  get:
+ *    tags:
+ *      - UserList
+ *    description: En este endpoint se traen todas las listas del usuario
+ *    produces:
+ *    - applications/json
+ *    parameters:
+ *    - in: header
+ *      name: Authorization
+ *      required: true
+ *    responses:
+ *      200:
+ *        description: Si el usuario tiene listas creadas se devuelve el siguiente objeto
+ *        schema:
+ *          properties:
+ *            estado:
+ *              type: boolean
+ *              example: true
+ *            data:
+ *              type: array
+ *              items:
+ *                $ref: '#/definitions/UserList'
+ *            mensaje:
+ *              type: string
+ *              example: null
+ *      400:
+ *        description: Si el usuario no tiene listas creadas se devuelve el siguiente objeto
+ *        schema:
+ *          properties:
+ *            estado:
+ *              type: boolean
+ *              example: false
+ *            data:
+ *              type: array
+ *              items:
+ *                type: string
+ *                example: 'Array vacio'
+ *            mensaje:
+ *              type: string
+ *              example: 'No se encuentran listas creadas'
+ */
 routesUserListApp.get('', isClient, async (request, response) => {
   const user = request.User.id
   const search = await UserListController.all({ user })
   response.json(search)
 })
 
+/**
+ * @swagger
+ * /app/userlist/{id}):
+ *  get:
+ *    tags:
+ *      - UserList
+ *    description: En este endpoint se detalla una lista del usuario en sesion
+ *    produces:
+ *    - applications/json
+ *    parameters:
+ *    - in: header
+ *      name: Authorization
+ *      required: true
+ *    - in: path
+ *      name: id
+ *      required: true
+ *    responses:
+ *      200:
+ *        description: Si la lista existe se devuelve el detalle dentro del siguiente objeto
+ *        schema:
+ *          properties:
+ *            estado:
+ *              type: boolean
+ *              example: true
+ *            data:
+ *              $ref: '#/definitions/UserList'
+ *            mensaje:
+ *              type: string
+ *              example: null
+ *      400:
+ *        description: Si la lista no existe
+ *        schema:
+ *          properties:
+ *            estado:
+ *              type: boolean
+ *              example: false
+ *            data:
+ *              type: array
+ *              items:
+ *                type: string
+ *                example: 'Array vacio'
+ *            mensaje:
+ *              type: string
+ *              example: 'No existe la lista de usuario'
+ */
 routesUserListApp.get('/:id', isClient, async (request, response) => {
   const _id = request.params.id
   const detail = await UserListController.detail({ _id })
