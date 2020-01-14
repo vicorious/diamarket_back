2c36c02c2f6e357cf261db403d2ee04adc3298b7
NicolasSalazar <desarrollo@gracialab.co>
2020-01-14 10:29:52 -0500

    pay controller and validation permits token

---
+++ controllers/payUController.js
@@ -0,0 +1,9 @@
+'use strict'
+// const urlDev = 'https://sandbox.api.payulatam.com/payments-api/4.0/service.cgi'
+// const urlProd = 'https://api.payulatam.com/payments-api/4.0/service.cgi'
+
+class PayU {
+
+}
+
+module.exports = new PayU()
--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const http = require('http')
 const morgan = require('morgan')
 const cors = require('cors')
 const routeUser = require('./urls/routes')
+
 const port = 5002
 const app = asyncify(express())
 const server = http.createServer(app)
-const passport = require('passport')
 
-app.use(passport.initialize())
-app.use(passport.session())
 app.use(cors())
 app.use(morgan('combined'))
 app.use(bodyParser.json({ extended: true, limit: '20000mb' }))
@@ -22,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '20000mb' }))
 app.use('/v1', routeUser)
 
 server.listen(port, () => {
-    console.log(`${chalk.green('[diamarket-account]')} server listening on port ${port}`)
+  console.log(`${chalk.green('[diamarket-account]')} server listening on port ${port}`)
 })
 
 module.exports = server
